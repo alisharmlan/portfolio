dd8ff6654aa7c83a3ee380e1544948287e557d33
batmanlovegaming@gmail.com <batmanlovegaming@gmail.com>
2024-07-19 22:51:07 +0800

    updated fetch function

--- src/pages/RecommendedProducts.tsx
+++ src/pages/RecommendedProducts.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import getDocuments from "@/firebase/firestore/getData";
 import { QueryDocumentSnapshot } from 'firebase/firestore';
 import { useAuthContext } from '@/context/AuthContext';
@@ -11,26 +11,82 @@ function RecommendedProducts() {
       const user = searchParams?.get("user");
 
     const [products, setProducts] = useState<QueryDocumentSnapshot[]>();
-    const fetchAllProducts = async () => {
+    const fetchAllProducts = useCallback(async () => {
+      try{
         const {result, error} = await getDocuments("products")
         if(result == null) return;
-        setProducts(result.docs)
-    }
+        setProducts(result.docs);
+        if(products)
+        await console.log(products[0].data());
+      }
+      catch {
+        (e: any) => {console.log(e)}
+      }
+    }, [products]) 
+        
+        
     
-    const [userData, setUserData] = useState<User>();
-
+    const [userData, setUserData] = useState<User>(); // the data from the user used for the comparison
     useEffect(() => {
-        fetchAllProducts();
-        (async () => {
-          const data = await fetchUser(user);
-          setUserData(data);
-        })();
-    }, [user])
+      if(!user){
+        return;
+      }
+      
+       if(!products) fetchAllProducts();
+
+      (async () => {
+        const data: User | undefined = await fetchUser(user);
+        setUserData(data);
+        await CalculateCompatibility(data);
+      })();
+        
+    }, [user, fetchAllProducts, products])
+
+    const [calcLoading, setCalcLoading] = useState<boolean>(true);
+    const CalculateCompatibility = async (data?: User) => {
+      if(!data) return;
+
+      const {name, preferences} = data;
+
+      console.log(`User ${name.stringValue.toString()} is being calculated!`);
+      
+      // when all is completed
+      setCalcLoading(false);
+    }
     
     return (
       <div className="flex justify-center items-center h-screen w-screen bg-gradient-to-br from-[#fc8f83] to-[#f7ccc8]">
         <button onClick={fetchAllProducts}>Get All Products</button>
-        hi : {userData?.name.stringValue}
+        {
+          user != null || undefined ? (
+            // if the user exist
+            <>
+              {
+                calcLoading ? (
+                  // if calculation still loading
+                  <> 
+                    <h1>
+                      loading...
+                    </h1>
+                  </>
+                ) : (
+                  // if done calculation
+                  <>
+                    <h1>
+                      {products ? (<>hi: {products[0].data().brand}</>) : (<></>)
+                      }
+                    </h1>
+                  </>
+                )
+              } 
+            </>
+        ): (
+          // If the user doesn't exist
+          <>
+            <h1> Error user not found!</h1>
+          </>
+        )
+      }
       </div>
     );
 }
